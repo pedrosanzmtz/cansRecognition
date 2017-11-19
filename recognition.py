a71b82de13426108ce9fdd3b949ffd04cbe0a7c9
pedrosanzmtz <pedrosanzmtz@gmail.com>
2017-11-19 08:21:58 -0600

    parse base64 to np

--- recognition.py
+++ recognition.py
@@ -10,11 +10,13 @@ values = ['pepsi', 'manzanita', '7up']
 def predict(data):
 	img = np.fromstring(data, np.uint8)
 	img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+	#print(img.shape)
 	img = cv2.resize(img, (32, 32), interpolation=cv2.INTER_AREA)
 	img = img.reshape(1, 3, 32, 32)
 	img = img / 255.0
 	img = img.astype('float32')
-	result = model.predict(img)
+	result = np.round(model.predict(img))
+	#print(result)
 	try:
 		uid = int(np.where(result[0]==1)[0])
 		result = {'result': values[uid]}
